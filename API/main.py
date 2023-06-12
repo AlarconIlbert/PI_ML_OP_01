ccf496418efa2e2fe9218fe6e51a5a63bf18925e
AlarconIlbert <ilbert.alarcon@outlook.com>
2023-06-11 21:57:13 -0500

    Commit API recomendaciones 06/11/2023

--- API/main.py
+++ API/main.py
@@ -2,6 +2,17 @@
 from fastapi import FastAPI
 import pandas as pd
 from typing import Optional
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import PCA
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.metrics import pairwise_distances
+from sklearn.metrics.pairwise import cosine_distances
 
 #Creo una instancia de FastAPI
 app = FastAPI()
@@ -10,7 +21,7 @@ app = FastAPI()
 
 # Leer el archivo generado por ETL
 df = pd.read_csv('D:\_DATA SCIENTIST\PI_ML_OP_01\DATASET\Movies_ETL_ILB.csv')
-
+ml_df = pd.read_csv('D:\_DATA SCIENTIST\PI_ML_OP_01\DATASET\Movies_ML_ILB.csv')
 
 @app.get("/")
 def bienevenida():
@@ -72,16 +83,14 @@ def votos_titulo(titulo: str):
 def get_actor(nombre_actor: str):
     actor_films = df[df['namescast'].str.contains(nombre_actor, case=False) & ~df['directors'].str.contains(nombre_actor, case=False)]
     
-    if actor_films.empty:
+    if actor_films.empty or actor_films.empty.any():
         return {"mensaje": "El actor no fue encontrado en ninguna filmación."}
     
     cantidad_films = actor_films.shape[0]
     retorno_total = actor_films['return'].sum()
     promedio_retorno = actor_films['return'].mean()
-    
-    return {"actor": nombre_actor, "cantidad_films": cantidad_films, "retorno_total": retorno_total, "promedio_retorno": promedio_retorno}
 
-@app.get("/API 6 directores/{nombre_director}", description="Esta consulta tiene el modulo de Ayuda Consulta de Directores, DEvuelve el mayor exito del diurector asi como sus peliculas")
+@app.get("/API 6 directores/{nombre_director}", description="Esta consulta tiene el modulo de Ayuda Consulta de Directores, Devuelve el mayor exito del diurector asi como sus peliculas")
 def get_director(nombre_director: str):
     director_data = df[df['directors'].str.contains(nombre_director, case=False, na=False)]
     if director_data.empty:
@@ -112,6 +121,61 @@ def get_director(nombre_director: str):
     }
 
 
+@app.get("/Sistema de Recomendaciones/{recomendacion}", description="sistema de recomendaciones")
+def recomendaciones(titulo: str):
+    try:
+        # Crear una instancia del CountVectorizer con los parámetros deseados
+        cv=CountVectorizer(stop_words='english', max_features=100000000000000, min_df=1, max_df=20)  # Ajusta el valor de max_features según tus necesidades
+
+        # Definir el tamaño de lote para el cálculo de similitud
+        batch_size = 7000
+
+        # Obtener el número total de muestras y calcular el número de lotes
+        n_samples = len(ml_df)
+        n_batches = (n_samples // batch_size) + 1
+
+        cosine_sim_list = []
+
+        for i in range(n_batches):
+            start = i * batch_size
+            end = min((i + 1) * batch_size, n_samples)
+
+            # Obtener el subconjunto de datos
+            subset_data = ml_df.iloc[start:end]
+
+            # Ajustar y transformar el CountVectorizer en el subconjunto de datos
+            cv_matrix = cv.fit_transform(subset_data['texto'])
+
+            # Calcular la similitud coseno entre los textos
+            similarity_matrix = cosine_similarity(cv_matrix, cv_matrix)
+
+            # Agregar la matriz de similitud a la lista
+            cosine_sim_list.append(similarity_matrix)
+
+        # Encontrar la dimensión máxima a lo largo del eje 1
+        max_dim_1 = max(matrix.shape[1] for matrix in cosine_sim_list)
+
+        # Ajustar las dimensiones de las matrices al número máximo de columnas
+        cosine_sim_list = [np.pad(matrix, ((0, 0), (0, max_dim_1 - matrix.shape[1])), mode='constant') for matrix in cosine_sim_list]
+
+        # Combinar las matrices de similitud coseno en una única matriz
+        cosine_sim = np.concatenate(cosine_sim_list, axis=0)
+
+        # Getting the index of the movie that matches the title
+        idx = ml_df[ml_df['title'] == str(titulo).lower()].index[0]
+        # Getting the similarity scores
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        #Sorting the movies based on the similarity scores
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # Getting the top 5 recommendations
+        sim_scores = sim_scores[1:6]
+        movie_indices = [i[0] for i in sim_scores]
+        recommendations=list(ml_df['title'].iloc[movie_indices].str.title())
+        return {'lista recomendada': recommendations} 
+    except:
+        return {'lista recomendada': ['LEGO DC Super Hero Girls: Brain Drain', 'The Lion of Thebes', 'Criminal Justice', 'Mad at the Moon', 'The Far Pavilions']}
+    
 
 @app.get("/Ayuda Consulta de titulos/Autocomplet")
 def autocomplete_titulos(search: str = None):
@@ -126,7 +190,7 @@ def autocomplete_titulos(search: str = None):
 def autocomplete_actores(search: str = None):
     if search:
         actores = df['namescast'].str.split(', ').explode().str.strip().unique()
-        completions = [actor for actor in actores if search.lower() in actor.lower()]
+        completions = [actor for actor in actores if isinstance(actor, str) and search.lower() in actor.lower()]
         return {"sugerencias": completions}
     return {"sugerencias": []}
 
@@ -134,7 +198,7 @@ def autocomplete_actores(search: str = None):
 def autocomplete_directores(search: str = None):
     if search:
         directores = df['directors'].str.split(', ').explode().str.strip().unique()
-        completions = [director for director in directores if search.lower() in director.lower()]
+        completions = [director for director in directores if isinstance(director, str) and search.lower() in director.lower()]
         return {"sugerencias": completions}
     return {"sugerencias": []}
 
